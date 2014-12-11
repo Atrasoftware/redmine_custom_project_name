8aadb37660ce1cf72e21b728c821fc58a6e1b509
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-11 09:39:21 +0100

    add settings

--- init.rb
+++ init.rb
@@ -4,6 +4,8 @@ Redmine::Plugin.register :redmine_custom_project_name do
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'https://github.com/Atrasoftware/redmine_custom_project_name'
+
+  settings :partial => 'settings/setting_rcpn'
 end
 Rails.application.config.to_prepare do
   Query.send(:include, Patches::QueryPatch)
