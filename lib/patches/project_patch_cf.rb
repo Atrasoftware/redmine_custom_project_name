f37db90de1379d5223699535d6c76420b3683395
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-01-29 15:41:09 +0100

    performance

--- lib/patches/project_patch_cf.rb
+++ lib/patches/project_patch_cf.rb
@@ -21,6 +21,7 @@ module  Patches
       output = ["#{identifier}"]
   if settings.present?
     cfs= ApplicationHelper.get_sorted_cf(settings)
+
     if cfs.present?
       cfs.select{|col| settings[col.name] }.each do |cf|
         visible_custom_field_values.select{|coll| coll.custom_field.name == cf.name }.each do |custom_value|
