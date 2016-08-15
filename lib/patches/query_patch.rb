37aa329f2c613ac27b962a85b84f8053bcb8e44a
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-15 19:00:11 +0100

    fix bugs

--- lib/patches/query_patch.rb
+++ lib/patches/query_patch.rb
@@ -21,17 +21,8 @@ module InstanceMethods
   def all_projects_values_with_modif
     return @all_projects_values if @all_projects_values
     values = []
-   #app_helper_get_sorted_cf = ApplicationHelper.get_sorted_cf(Setting.send "plugin_redmine_custom_project_name").map(&:name)
     Project.project_tree(all_projects) do |p, level|
       prefix = (level > 0 ? ('--' * level + ' ') : '')
-=begin
-    output = ["#{p.identifier}"]
-    app_helper_get_sorted_cf.each do |cf|
-      p.visible_custom_field_values.select{|coll| coll.custom_field.name == cf }.each do |custom_value|
-        output<< custom_value.value unless custom_value.value.blank?
-      end
-     end
-=end
       values << ["#{prefix}#{p.to_s}", p.id.to_s]
     end
     @all_projects_values = values
